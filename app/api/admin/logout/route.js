4dd2a99487c399c744b69de72e9187a3fa9261f6
Sahil Srivastav <user.kanxer@gmail.com>
2025-11-27 01:13:15 +0530

    Update route.js

--- app/api/admin/logout/route.js
+++ app/api/admin/logout/route.js
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  const res = NextResponse.redirect(new URL("/admin/login", process.env.NEXT_PUBLIC_BASE_URL || "https://hackncode-nextjs.vercel.app"));
+  const res = NextResponse.redirect(new URL("/admin/login", process.env.NEXT_PUBLIC_BASE_URL));
 
   res.cookies.set("admin_session", "", {
     maxAge: 0,
@@ -10,5 +10,6 @@ export async function GET() {
 
   return res;
 }
+
 
 
