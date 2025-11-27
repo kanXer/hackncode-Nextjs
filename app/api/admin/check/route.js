e4e24b751395dc36a6509d2a78d710e232b57249
Sahil Srivastav <user.kanxer@gmail.com>
2025-11-27 17:51:44 +0530

    Delete app/api/admin directory

--- app/api/admin/check/route.js
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-
-export async function GET(req) {
-  const session = req.cookies.get("admin_session");
-
-  return NextResponse.json({
-    loggedIn: !!session
-  });
-}
