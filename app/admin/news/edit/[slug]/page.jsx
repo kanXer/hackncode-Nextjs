90d5937a5ad078a6fa94beef4dd3104ed53938b3
Sahil Srivastav <user.kanxer@gmail.com>
2025-11-27 17:50:21 +0530

    Delete app/admin directory

--- app/admin/news/edit/[slug]/page.jsx
+++
@@ -1,8 +0,0 @@
-import EditNewsClient from "./EditNewsClient";
-import "./assets/styles.css"
-export default async function Page({ params }) {
-  const { slug } = await params; // <-- THIS FIXES IT
-
-  return <EditNewsClient slug={slug} />;
-}
-
