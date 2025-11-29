cdd5b7d6d11a7287f8851eee341991fb7cd47807
Sahil Srivastav <user.kanxer@gmail.com>
2025-11-29 17:39:48 +0530

    Update and rename app/news/page.js to app/news/[[...all]]/page.js

--- app/news/[[...all]]/page.js
+++ app/news/[[...all]]/page.js
@@ -1,4 +1,3 @@
-export const dynamicParams = true;
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
 export const fetchCache = "force-no-store";
@@ -49,4 +48,5 @@ export default async function NewsPage({ searchParams }) {
     </div>
   );
 }
+
 
