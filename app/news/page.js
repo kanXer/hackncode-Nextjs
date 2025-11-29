2049ca3b97ac45b0c05f5e7fa30ab9b36e623e39
Sahil Srivastav <user.kanxer@gmail.com>
2025-11-29 17:36:32 +0530

    Update page.js

--- app/news/page.js
+++ app/news/page.js
@@ -1,3 +1,4 @@
+export const dynamicParams = true;
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
 export const fetchCache = "force-no-store";
@@ -48,3 +49,4 @@ export default async function NewsPage({ searchParams }) {
     </div>
   );
 }
+
